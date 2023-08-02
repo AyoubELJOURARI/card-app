69927a64b861166f8fad04f4641e87231d431b2f
AD\chgv7135 <ayoub.eljourari@orange.com>
2023-08-02 18:06:23 +0200

    Create reusable profileCard component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import ProfileCard from './ProfileCard';
+import AlexaImage from './images/alexa.png'
+import SiriImage from './images/siri.png'
+import CortanaImage from './images/cortana.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <ProfileCard />
-        <ProfileCard />
-        <ProfileCard />
-      </header>
+
+        <div className='d-flex justify-content-evenly mx-auto'>
+          <ProfileCard  title="First_USER" handle="@ALEXA_1Id" image={AlexaImage} description='Alexa was created by Amazon.'/>
+          <ProfileCard  title="Second_USER" handle="@CORTANA_2Id" image={CortanaImage} description='Cortana was created by Microsoft.'/>
+          <ProfileCard  title="Third_USER" handle="@SIRI_3Id" image={SiriImage} description='Siri was created by Apple.'/>
+        </div>
+        
     </div>
   );
 }
--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,5 +1,15 @@
-function ProfileCard() {
-    return <div>Profile Card</div>
+function ProfileCard({ title, handle, image, description }) {
+    return (
+        <div className="card">
+            <img src={image} class="card-img-top" alt="logo"/>
+            <div class="card-body">
+                <h5 class="card-title">{title}</h5>
+                <h6 class="card-subtitle">Handle is {handle}.</h6>
+                <p class="card-text">{description}</p>
+            </div>
+        </div>
+    
+    )
 }
 
 export default ProfileCard;
